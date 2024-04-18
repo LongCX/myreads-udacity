5210c7aedfa335b824bff98013e86694ac459643
LongCX <dualcore1995@gmail.com>
2024-04-18 22:44:09 +0700

    refactor: book shelf title

--- src/components/BookShelf.js
+++ src/components/BookShelf.js
@@ -3,9 +3,11 @@ import { Link } from "react-router-dom";
 import Book from "./Book"
 
 const BookShelf = ({ books, updateBookShelf }) => {
-    const shelves = ['currentlyReading', 'wantToRead', 'read'];
-    const shelveName = ['Currently Reading', 'Want to Read', 'Read'];
-
+    const bookShelfTitles = [
+        { type: "currentlyReading", title: "Currently Reading" },
+        { type: "wantToRead", title: "Want to Read" },
+        { type: "read", title: "Read" },
+    ];
 
     return (
         <div className="list-books">
@@ -13,16 +15,16 @@ const BookShelf = ({ books, updateBookShelf }) => {
                 <h1>MyReads</h1>
             </div>
             {
-                shelves.map((shelf, index) => {
+                bookShelfTitles.map((bookShelfTitle, index) => {
                     return (
                         <div className="list-books-content" key={index}>
                             <div>
                                 <div className="bookshelf">
-                                    <h2 className="bookshelf-title">{shelveName[index]}</h2>
+                                    <h2 className="bookshelf-title">{bookShelfTitle.title}</h2>
                                     <div className="bookshelf-books">
                                         <ol className="books-grid">
                                             {
-                                                books && books.length ? books.filter((book) => book.shelf === shelf).map((book) => (
+                                                books && books.length ? books.filter((book) => book.shelf === bookShelfTitle.type).map((book) => (
                                                     <li key={book.id}>
                                                         <Book book={book} updateBookShelf={updateBookShelf} />
                                                     </li>
