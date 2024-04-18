38706e59805ba811564576a5a2201a5fb034c920
LongCX <dualcore1995@gmail.com>
2024-04-18 22:36:14 +0700

    fix: bug can't update book status currentlyReading

--- src/components/Book.js
+++ src/components/Book.js
@@ -15,13 +15,9 @@ const Book = ({ book, updateBookShelf }) => {
                     }}
                 ></div>
                 <div className="book-shelf-changer">
-                    <select value={book.shelf} onChange={(e) => updateBookShelf(book, e.target.value)}>
-                        <option value="none" disabled>
-                            Move to...
-                        </option>
-                        <option value="currentlyReading">
-                            Currently Reading
-                        </option>
+                    <select value={book.shelf ? book.shelf : 'none'} onChange={(e) => updateBookShelf(book, e.target.value)}>
+                        <option value="moveTo" disabled>Move to...</option>
+                        <option value="currentlyReading">Currently Reading</option>
                         <option value="wantToRead">Want to Read</option>
                         <option value="read">Read</option>
                         <option value="none">None</option>
